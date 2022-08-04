5e1770b8db69a4e1123dcba10fff852d456d3981
JuanDemera <57873984+JuanDemera@users.noreply.github.com>
2022-08-03 23:07:13 -0500

    Update MainDM.java
    posible lectura del arbol en mainDm

--- P_Grupo06/src/main/java/data/MainDM.java
+++ P_Grupo06/src/main/java/data/MainDM.java
@@ -5,6 +5,8 @@
 package data;
 
 import com.mycompany.p_grupo06.App;
+import java.util.Scanner;
+import tree.BinaryTree;
 
 /**
  *
@@ -14,7 +16,19 @@ import com.mycompany.p_grupo06.App;
 public class MainDM {
     
     public static void main(String args[]){
-        DataManager dm = new DataManager(App.pathArchivo);
+        BinaryTree prueba=new BinaryTree();
+        prueba.setRootContent("pregunta1", 0);
+        prueba.setLeft(new BinaryTree("pregunta2",1));
+        prueba.setRight(new BinaryTree("pregunta2",1));
+        prueba.getLeft().setLeft(new BinaryTree("oso",2));
+        prueba.getLeft().setRight(new BinaryTree("perro",2));
+        prueba.getRight().setLeft(new BinaryTree("perro2",2));
+        prueba.getRight().setRight(new BinaryTree("oso2",2));
+        
+        
+        
+        menu(prueba);
+        /*DataManager dm = new DataManager(App.pathArchivo);
         System.out.println(dm.attributes);
         System.out.println(dm.recordMatrix);
         int n1 = 1;
@@ -22,6 +36,50 @@ public class MainDM {
         System.out.println(n);
         n1++;
         n = (int)Math.pow(5, n1);
-        System.out.println(n);
+        System.out.println(n);*/
     }
+    
+    public static void menu(BinaryTree t){
+        int options;
+        Scanner input= new Scanner(System.in);
+        System.out.println("----------Ingrese una opcion----------\n1. Start\n2. Salir ");
+        options = input.nextInt();
+        switch(options){
+            case 1:
+                questionsDisplay(t);
+                break;
+            case 2:
+                System.exit(0);
+            default:
+                System.out.println("Opcion Invalida\n3");
+                menu(t);
+        }    
+    }
+    
+    public static void questionsDisplay(BinaryTree root){
+        if(root.getRight()!=null && root.getLeft()!=null){
+            System.out.print("Tu Animal es: ");
+            System.out.println(root.getRootPregunta());
+            Scanner input= new Scanner(System.in);
+            String answer=input.nextLine();
+            
+            if(answer.equalsIgnoreCase("Y")){
+                if(root.getLeft()!=null){
+                    questionsDisplay(root.getLeft());
+                }
+                
+            }else if(answer.equalsIgnoreCase("N")){
+                if(root.getRight()!=null){
+                    questionsDisplay(root.getRight());
+                }
+            }else{
+                System.out.println("Responda con Y/N");
+                questionsDisplay(root);
+            }
+        }else{
+            System.out.println("Estas pensando en un: ");
+            System.out.println(root.getRootPregunta());
+        }    
+                
+    }   
 }
